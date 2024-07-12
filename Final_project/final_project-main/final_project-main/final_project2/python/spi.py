12899d75abd6787e3df1c79f7e166df9810741e8
KONG WANGTA <kongnapha25@gmail.com>
2024-07-12 15:46:57 +0700

    Update file path

--- Final_project/final_project-main/final_project-main/final_project2/python/spi.py
+++ Final_project/final_project-main/final_project-main/final_project2/python/spi.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the Excel file using raw string to avoid unicode escape errors
-file_path = r'C:\Users\ASUS\Documents\Final_project\final_project-main\final_project-main\final_project2\data\save_rain_clean_excel\sumfile_rain\merge_files_rain_data.xlsx'
+file_path = r'data/save_rain_clean_excel/sumfile_rain/combined_rain_data.xlsx'
 rain_data = pd.read_excel(file_path, sheet_name='Sheet1')
 
 # Ensure 'sum' column is numeric
